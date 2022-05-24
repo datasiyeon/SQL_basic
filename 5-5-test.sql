22be7884820c0392d248fde051cb65fc70aa5f3a
datasiyeon <102789613+datasiyeon@users.noreply.github.com>
2022-05-24 21:10:52 +0900

    Add files via upload

---
+++ 5-5-test.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS purchase_log_with_coupon;
+CREATE TABLE purchase_log_with_coupon (
+    purchase_id varchar(255)
+  , amount      integer
+  , coupon      integer
+);
+
+INSERT INTO purchase_log_with_coupon
+VALUES
+    ('10001', 3280, NULL)
+  , ('10002', 4650,  500)
+  , ('10003', 3870, NULL)
+;
+SELECT
+	purchase_id
+	, amount
+	, coupon
+	, amount - coupon AS discount_amount1
+	, amount - COALESCE(coupon, 0) AS discount_amount2 -- COALESCE 함수로 NULL -> 0으로 대치
+FROM
+	purchase_log_with_coupon
+;
